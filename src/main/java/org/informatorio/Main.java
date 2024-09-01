9299ad2c85dfa80443c9701bbc0398b79bdf31e7
mfabri31 <fabriciomedina9531@gmail.com>
2024-09-01 01:02:19 -0300

    Agrega menu de opciones

--- src/main/java/org/informatorio/Main.java
+++ src/main/java/org/informatorio/Main.java
@@ -1,12 +1,45 @@
 package org.informatorio;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("===== Gestor de Eventos =====");
+        System.out.println("===== Gestor de Eventos =====");
 
+        int option;
 
+        Scanner scann = new Scanner(System.in);
+
+        do {
+            System.out.println("[1] Crear evento");
+            System.out.println("[2] Agregar participante");
+            System.out.println("[3] Listar eventos");
+            System.out.println("[4] Salir");
+
+            option = scann.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println("Creando evento");
+                    break;
+                case 2:
+                    System.out.println("Agregando participante");
+                    break;
+                case 3:
+                    System.out.println("Listando eventos");
+                    break;
+                case 4:
+                    System.out.println("Saliendo");
+                    break;
+
+                default:
+                    System.out.println("Opción no válida!.");
+                    break;
+            }
+
+        } while (option != 4);
+
+        scann.close();
     }
 }
